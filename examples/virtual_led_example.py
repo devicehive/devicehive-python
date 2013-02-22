eca5cd4e7339b29b486bc3c2b76f820eaeb72f67
zolkko <zolkko@gmail.com>
2013-02-22 14:04:47 +0400

    virtual_led_example has been switched to libraries new version

--- examples/virtual_led_example.py
+++ examples/virtual_led_example.py
@@ -1,113 +1,132 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:set et tabstop=4 shiftwidth=4 nu nowrap fileencoding=utf-8:
 
 import sys
-import signal
-import functools
-from os import path
-from ConfigParser import ConfigParser as Conf
+import os
 from twisted.python import log
 from twisted.internet import reactor
+from zope.interface import implements
+from ConfigParser import ConfigParser as Conf
+try:
+    import devicehive
+except :
+    sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import devicehive
+import devicehive.poll
+import devicehive.ws
+import devicehive.auto
+import devicehive.interfaces
 
 
-class VirtualLedInfos(object):
-	"""
-	Use duck typing to describe virtual device insterd of
-	overriding methods in L{DeviceDelegate} subclass.
-	"""
-	def __init__(self, config):
-		"""
-		All device description is contained in configuration file and
-		an instance of ConfigParser is used to read it.
-		"""
-		self.config = config
-	def device_id(self):
-		return self.config.get('device', 'id')
-	def device_key(self):
-		return self.config.get('device', 'key')
-	def device_name(self):
-		return self.config.get('device', 'name')
-	def device_status(self):
-		return 'Online'
-	def network_name(self):
-		return self.config.get('network', 'name')
-	def network_description(self):
-		return self.config.get('network', 'description')
-	def device_class_name(self):
-		return self.config.get('device_class', 'name')
-	def device_class_version(self):
-		return self.config.get('device_class', 'version')
-	def device_class_is_permanent(self):
-		return False
-	def equipment(self):
-		return [devicehive.Equipment(name = 'VirtualLED', code = 'LED', _type = 'Controllable LED'),]
+class VirtualLedInfo(object):
+    
+    implements(devicehive.interfaces.IDeviceInfo)
+    
+    def __init__(self, config):
+        self.config = config
+    
+    @property
+    def id(self):
+        return self.config.get('device', 'id')
+    
+    @property
+    def key(self):
+        return self.config.get('device', 'key')
+    
+    @property
+    def name(self):
+        return self.config.get('device', 'name')
+    
+    @property
+    def status(self):
+        return 'Online'
+    
+    @property
+    def data(self):
+        return None
+    
+    @property
+    def network(self):
+        return devicehive.Network(key = self.config.get('network', 'name'),
+                                  name = self.config.get('network', 'name'),
+                                  descr = self.config.get('network', 'description'))
+    
+    @property
+    def device_class(self):
+        return devicehive.DeviceClass(name = self.config.get('device_class', 'name'),
+                    version = self.config.get('device_class', 'version'))
+    
+    @property
+    def equipment(self):
+        return [devicehive.Equipment(name = 'VirtualLED', code = 'LED', type = 'Controllable LED')]
 
 
-class VirtualLedDelegate(VirtualLedInfos, devicehive.DeviceDelegate):
-	"""
-	A subclass of DeviceDelegate class.
-	"""
-	def __init__(self, config):
-		super(VirtualLedDelegate, self).__init__(config)
-		self._led_state = 0
-
-	def status_notify(self):
-		self.notify('equipment', equipment = 'LED', state = self._led_state)
-
-	def do_update_led_state(self, finish_deferred, equipment = None, state = 0):
-		if equipment == 'LED':
-			self._led_state = state
-			self.status_notify()
-			finish_deferred.callback(devicehive.CommandResult('Completed'))
-		else :
-			finish_deferred.errback(NotImplementedError('Unknown equipment {0}.'.format(equipment)))
-
-	def do_command(self, command, finish_deferred):
-		if command['command'] == 'UpdateLedState':
-			params = command['parameters']
-			self.do_update_led_state(finish_deferred, **params)
-		else :
-			finish_deferred.errback(NotImplementedError('Unknown command {0}.'.format(command['command'])))
-
-
-def initialize_logging():
-	"""
-	Initialize logging. Logs will be printed into standard output
-	"""
-	log.startLogging(sys.stdout)
-
-
-def signal_handler(signum, e, dev=None):
-	log.msg("Terminating reactor.")
-	reactor.stop()
-
-
-def init_signal_handlers(dev):
-	sig_func = functools.partial(signal_handler, dev = dev)
-	if hasattr(signal, "SIGTERM"):
-		signal.signal(signal.SIGTERM, sig_func)
-	if hasattr(signal, "SIGINT"):
-		signal.signal(signal.SIGINT, sig_func)
-	if hasattr(signal, "SIGHUP"):
-		signal.signal(signal.SIGHUP, sig_func)
+class VirtualLedApp(object):
+    
+    implements(devicehive.interfaces.IProtoHandler)
+    
+    def __init__(self, config):
+        self.factory = None
+        self.info = VirtualLedInfo(config)
+        self.connected = False
+        self.led_state = 0
+    
+    def on_apimeta(self, websocket_server, server_time):
+        pass
+    
+    def on_closing_connection(self):
+        pass
+    
+    def on_connection_failed(self, reason):
+        pass
+    
+    def on_failure(self, device_id, reason):
+        pass
+    
+    def on_connected(self):
+        def on_subscribe(result) :
+            self.connected = True
+            self.factory.subscribe(self.info.id, self.info.key)
+        def on_failed(reason) :
+            log.err('Failed to save device {0}. Reason: {1}.'.format(self.info, reason))
+        self.factory.device_save(self.info).addCallbacks(on_subscribe, on_failed)
+    
+    def on_command(self, device_id, command, finished):
+        if command.command == 'UpdateLedState':
+            self.do_update_led_state(finished, **command.parameters)
+        else :
+            finished.errback(NotImplementedError('Unknown command {0}.'.format(command.command)))
+    
+    def do_update_led_state(self, finish_deferred, equipment = None, state = 0):
+        if equipment == 'LED':
+            self.led_state = state
+            self.status_notify()
+            finish_deferred.callback(devicehive.CommandResult('Completed'))
+        else :
+            finish_deferred.errback(NotImplementedError('Unknown equipment {0}.'.format(equipment)))
+    
+    def status_notify(self):
+        if self.connected :
+            self.factory.notify('equipment', {'equipment': 'LED', 'state': self.led_state}, self.info.id, self.info.key)
 
 
 if __name__ == '__main__':
-	initialize_logging()
-	# read conf-file
-	conf = Conf()
-	conf.read(path.join(path.dirname(__file__), path.splitext(path.basename(__file__))[0] + ".cfg"))
-	# create device-delegate instance
-	virt_led = VirtualLedDelegate(conf)
-	# install signal handlers
-	init_signal_handlers(virt_led)
-	# Default factory for HTTP11 protocol
-	virt_led_factory = devicehive.HTTP11DeviceHiveFactory(device_delegate = virt_led)
-	# Send notification right after registration
-	virt_led.status_notify()
-	# Connect to device-hive
-	reactor.connectDeviceHive("http://pg.devicehive.com/api/", virt_led_factory)
-	try :
-		reactor.run()
-	except KeyboardInterrupt, err:
-		reactor.stop()
+    log.startLogging(sys.stdout)
+    # read conf-file
+    conf = Conf()
+    conf.read(os.path.join(os.path.dirname(__file__), os.path.splitext(os.path.basename(__file__))[0] + '.cfg'))
+    # create device-delegate instance
+    virt_led = VirtualLedApp(conf)
+    # Automacti factory
+    # Also it is possible to use C{devicehive.poll.PollFactory} or C{devicehive.ws.WebSocketFactory}
+    virt_led_factory = devicehive.auto.AutoFactory(virt_led)
+    # Send notification right after registration
+    virt_led.status_notify()
+    # Connect to device-hive
+    reactor.connectDeviceHive("http://ecloud.devicehive.com/apiv7/", virt_led_factory)
+    try :
+        reactor.run()
+    except KeyboardInterrupt, err:
+        reactor.stop()
+
