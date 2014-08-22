77c18edff3016ebcd66abf7edc29c0882420ddfa
zolkko <zolkko@gmail.com>
2014-08-22 15:43:05 +0400

    LCD device has been added to the example

--- examples/raspi_led_thermo.py
+++ examples/raspi_led_thermo.py
@@ -16,12 +16,22 @@ import os
 import time
 from time import sleep
 
-try :
+
+try:
     import RPi.GPIO as GPIO
+
+    def get_i2c_file():
+        base_dir = '/sys/bus/w1/devices/'
+        device_folder = glob.glob(base_dir + '28*')
+        if not device_folder:
+            print 'Make sure that DS18B20 sensor is connected'
+            exit()
+        return device_folder[0] + '/w1_slave'
 except ImportError:
     class FakeGPIO(object):
         OUT = 'OUTPUT BCM.GPIO17'
         BOARD = 'BOARD'
+        BCM = 'BCM'
         def __init__(self):
             print 'Fake gpio initialized'
         def setmode(self, value):
@@ -32,6 +42,9 @@ except ImportError:
             print 'Set gpio {0}; Value: {1};'.format(io, vlaue)
     GPIO = FakeGPIO()
 
+    def get_i2c_file():
+        return '/dev/null'
+
 from zope.interface import implements
 from twisted.python import log
 from twisted.internet import reactor, task
@@ -40,31 +53,23 @@ import devicehive
 import devicehive.auto
 
 
-base_dir = '/sys/bus/w1/devices/'
-device_folder = glob.glob(base_dir + '28*')
-if not device_folder:
-    print 'Make sure that DS18B20 sensor is connected'
-    exit()
-device_file = device_folder[0] + '/w1_slave'
-
 # change it to match your address for 1-wire sensor
-_W1_FILENAME = device_file
-if not os.path.exists(_W1_FILENAME) :
-    _W1_FILENAME = '/dev/null'
+_W1_FILENAME = get_i2c_file()
 
 # Board's pin #11 (GPIO18)
-_LED_PIN=12
+_LED_PIN = 12
 
 # API URL (register for free playground at http://beta2.devicehive.com/playground
 _API_URL = 'http://pg.devicehive.com/api/'
 
-#
-# for easier reading, this class holds all registration information for DeviceHive
-#
+
 class RasPiConfig(object):
-    
+    """
+    for easier reading, this class holds all registration information for DeviceHive
+    """
+
     implements(devicehive.interfaces.IDeviceInfo)
-    
+
     @property
     def id(self):
         return '9f33566e-1f8f-11e2-8979-c42c030dd6a5'
@@ -83,15 +88,15 @@ class RasPiConfig(object):
     
     @property
     def network(self):
-        return devicehive.Network(key = 'Netname', name = 'Netname', descr = 'RasPi/Py LED/w1 sample')
+        return devicehive.Network(key='Netname', name='Netname', descr='RasPi/Py LED/w1 sample')
     
     @property
     def device_class(self):
-        return devicehive.DeviceClass(name = 'Class1', version = '1.0', is_permanent = False)
+        return devicehive.DeviceClass(name='Class1', version='1.0', is_permanent=False)
     
     @property
     def equipment(self):
-        return [devicehive.Equipment(name = 'LED', code = 'LED', type = 'Controllable LED'), devicehive.Equipment(name = 'THERMO', code = 'temp', type = 'TempSensor')]
+        return [devicehive.Equipment(name='LED', code='LED', type='Controllable LED'), devicehive.Equipment(name='THERMO', code='temp', type='TempSensor')]
     
     @property
     def data(self):
@@ -115,20 +120,22 @@ class RasPiConfig(object):
 
         return res
 
-#
-# This class handles DeviceHive API calls for our device
-#
+
 class RasPiApp(object):
+    """
+    This class handles DeviceHive API calls for our device
+    """
     
     implements(devicehive.interfaces.IProtoHandler)
     
-    def __init__(self, led, sensor):
+    def __init__(self, led, sensor, lcd):
         super(RasPiApp, self).__init__()
         self.connected = False
         self.notifs = []
         self.info = RasPiConfig()
         self.led = led
         self.sensor = sensor
+        self.lcd = lcd
     
     def on_apimeta(self, websocket_server, server_time):
         log.msg('on_apimeta')
@@ -167,11 +174,17 @@ class RasPiApp(object):
         # upon completion post the result back
         self.factory.notify('equipment', {'state': state, 'equipment': 'LED'}, device_id = self.info.id, device_key = self.info.key)
         finished.callback(devicehive.CommandResult('Completed'))
+
+    def do_set_command(self, finished, text=None):
+        self.lcd.write_string(text or '')
+        finished.callback(devicehive.CommandResult('Completed'))
     
     def on_command(self, device_id, command, finished):
         # Expecting command as 'UpdateState' and parameters as {"equipment" : "LED", "state" : "0"}
         if command.command == 'UpdateLedState' :
             self.do_short_command(finished,  **command.parameters)
+        elif command.command == 'set':
+            self.do_set_command(finished, **command.parameters)
         else :
             finished.errback()
         # end do_command
@@ -183,18 +196,21 @@ class RasPiApp(object):
             self.notifs.append({'notification': notif, 'parameters': params})
 
 
-#
-# Temperature sensor wrapper. Gets temperature readings form file, parses them
-# and notifies the services is the difference is greater than a certain threshold
-#
 class TempSensor(object):
+    """
+    Temperature sensor wrapper. Gets temperature readings form file, parses them
+    and notifies the services is the difference is greater than a certain threshold.
+    """
+
     def __init__(self, file_name):
         self.file_name = file_name
         self.last_temp = 0
         self.last_good_temp = 0
     
-    # internal, get temperature readings from device and check CRC
     def _get_temp(self):
+        """
+        internal, get temperature readings from device and check CRC
+        """
         with open(self.file_name) as f:
             content = f.readlines()
             for line in content:
@@ -207,51 +223,128 @@ class TempSensor(object):
                     return self.last_good_temp
         return 0.0
 
-    # check temperature, if greater than threshold, notify
     def get_temp(self, dev):
+        """
+        check temperature, if greater than threshold, notify
+        """
         temp = self._get_temp()
         if abs(temp - self.last_temp) > 0.2:
             log.msg('Temperature {0} -> {1}'.format(self.last_temp, temp))
             dev.notify('equipment', temperature = temp, equipment = "temp")
             self.last_temp = temp
 
-#
-# Wrapper from LED connected to RasPi
-#
+
 class LedDevice(object):
+    """
+    Wrapper from LED connected to RasPi
+    """
+
     def __init__(self, pin):
-        # We are using board PIN numbering (as opposed to chip's numbers)
+        """
+        We are using board PIN numbering (as opposed to chip's numbers)
+        """
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(pin, GPIO.OUT)
 
     def blink(self, count):
+        GPIO.setmode(GPIO.BOARD)
         for i in range(count):
-            GPIO.output(_LED_PIN,True)
+            GPIO.output(_LED_PIN, True)
             sleep(0.2)
-            GPIO.output(_LED_PIN,False)
+            GPIO.output(_LED_PIN, False)
             sleep(0.2)
 
     def set_on(self):
+        GPIO.setmode(GPIO.BOARD)
         GPIO.output(_LED_PIN, True)
 
     def set_off(self):
+        GPIO.setmode(GPIO.BOARD)
         GPIO.output(_LED_PIN, False)
 
-#
-# main
-#
+
+class Lcd(object):
+    LCD_RS = 23
+    LCD_E = 24
+    LCD_D4 = 25
+    LCD_D5 = 22
+    LCD_D6 = 17
+    LCD_D7 = 27
+    LCD_WIDTH = 20
+    LCD_CHR = True
+    LCD_CMD = False
+    LCD_LINE_1 = 0x80
+    LCD_LINE_2 = 0xC0
+    E_PULSE = 0.00005
+    E_DELAY = 0.00005
+
+    def __init__(self):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.LCD_E, GPIO.OUT)
+        GPIO.setup(self.LCD_RS, GPIO.OUT)
+        GPIO.setup(self.LCD_D4, GPIO.OUT)
+        GPIO.setup(self.LCD_D5, GPIO.OUT)
+        GPIO.setup(self.LCD_D6, GPIO.OUT)
+        GPIO.setup(self.LCD_D7, GPIO.OUT)
+        self.lcd_init()
+
+    def lcd_init(self):
+        self.write_byte(0x33, self.LCD_CMD)
+        self.write_byte(0x32, self.LCD_CMD)
+        self.write_byte(0x28, self.LCD_CMD)
+        self.write_byte(0x0C, self.LCD_CMD)
+        self.write_byte(0x06, self.LCD_CMD)
+        self.write_byte(0x01, self.LCD_CMD)
+
+    def toggle_enable(self):
+        time.sleep(self.E_DELAY)
+        GPIO.output(self.LCD_E, True)
+
+        time.sleep(self.E_PULSE)
+
+        GPIO.output(self.LCD_E, False)
+        time.sleep(self.E_DELAY)
+
+    def write_4bits(self, bits):
+        GPIO.output(self.LCD_D4, False)
+        GPIO.output(self.LCD_D5, False)
+        GPIO.output(self.LCD_D6, False)
+        GPIO.output(self.LCD_D7, False)
+
+        if bits & 0x01 == 0x01:
+            GPIO.output(self.LCD_D4, True)
+        if bits & 0x02 == 0x02:
+            GPIO.output(self.LCD_D5, True)
+        if bits & 0x04 == 0x04:
+            GPIO.output(self.LCD_D6, True)
+        if bits & 0x08 == 0x08:
+            GPIO.output(self.LCD_D7, True)
+
+    def write_byte(self, bits, mode):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.output(self.LCD_RS, mode)
+
+        self.write_4bits((bits & 0xf0) >> 4)
+        self.toggle_enable()
+
+        self.write_4bits(bits & 0x0f)
+        self.toggle_enable()
+
+    def write_string(self, message):
+        msg = message.ljust(self.LCD_WIDTH, ' ')[:self.LCD_WIDTH]
+        for ch in msg:
+            self.write_byte(ord(ch), self.LCD_CHR)
+
+
 if __name__ == '__main__' :
     log.startLogging(sys.stdout)
 
     led = LedDevice(_LED_PIN)
-    # Blink on start to ensure device is working
     led.blink(3)
+    lcd = Lcd()
+    temp_sensor = TempSensor(_W1_FILENAME)
 
-    # create temp sensor and queue it to check for temperature in a separate thread
-    tempSensor = TempSensor(_W1_FILENAME)
-
-    # create a delegate to handle commands
-    device = RasPiApp(led, tempSensor)
+    device = RasPiApp(led, temp_sensor, lcd)
 
     led_factory = devicehive.auto.AutoFactory(device)
     # led_factory = devicehive.poll.PollFactory(device)
