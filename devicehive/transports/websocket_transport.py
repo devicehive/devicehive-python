28097f2689897a516acc52dc960b9dd65ece7866
Dmitry Platon <platon.dimka@gmail.com>
2017-06-23 17:56:05 +0300

    Add send_request method

--- devicehive/transports/websocket_transport.py
+++ devicehive/transports/websocket_transport.py
@@ -85,13 +85,17 @@ class WebsocketTransport(BaseTransport):
         self._connect_thread.daemon = True
         self._connect_thread.start()
 
-    def request(self, action, obj, **params):
+    def send_request(self, action, obj, **params):
         self._assert_connected()
-        timeout = params.get('timeout', 30)
         obj_id = str(uuid.uuid1())
         obj['requestId'] = obj_id
         obj['action'] = action
         self._websocket.send(self._encode_obj(obj), opcode=self._data_opcode)
+        return obj_id
+
+    def request(self, action, obj, **params):
+        timeout = params.get('timeout', 30)
+        obj_id = self.send_request(action, obj, **params)
         send_time = time.time()
         while time.time() - timeout < send_time:
             obj = self._decode_data(self._websocket.recv())
