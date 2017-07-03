65ba02fae335b1831e8d2cc0d78f1efaca9e1d15
Dmitry Platon <platon.dimka@gmail.com>
2017-07-03 17:33:50 +0300

    Add transport Reques and Response classes

--- devicehive/transport.py
+++ devicehive/transport.py
@@ -6,3 +6,52 @@ def init(name, data_format, data_format_options, handler, handler_options):
                                                            data_format_options,
                                                            handler,
                                                            handler_options)
+
+
+class Request(object):
+    """Request class."""
+
+    def __init__(self, url, action, request, **params):
+        self._action = action
+        self._request = request
+        self._params = params
+        self._params['url'] = url
+
+    def action(self):
+        return self._action
+
+    def request(self):
+        return self._request
+
+    def params(self):
+        return self._params
+
+
+class Response(object):
+    """Response class."""
+
+    def __init__(self, response):
+        self._id = response.pop('requestId')
+        self._action = response.pop('action')
+        self._is_success = response.pop('status') == 'success'
+        self._code = response.pop('code', None)
+        self._error = response.pop('error', None)
+        self._data = response
+
+    def id(self):
+        return self._id
+
+    def action(self):
+        return self._action
+
+    def is_success(self):
+        return self._is_success
+
+    def code(self):
+        return self._code
+
+    def error(self):
+        return self._error
+
+    def data(self):
+        return self._data
