987a660f6adc86bd2eed176c5fa11cceebbcde9f
Dmitry Platon <platon.dimka@gmail.com>
2017-07-03 17:34:23 +0300

    Add Api class with refresh token request

---
+++ devicehive/api.py
@@ -0,0 +1,23 @@
+from devicehive.transport import Request
+from devicehive.transport import Response
+
+
+class Api(object):
+    """Api class."""
+
+    def __init__(self, transport):
+        self._transport = transport
+
+    def _request(self, url, action, request, **params):
+        req = Request(url, action, request, **params)
+        response = self._transport.request(req.action(), req.request(),
+                                           **req.params())
+        return Response(response)
+
+    def refresh_token(self, refresh_token):
+        url = 'token/refresh'
+        action = url
+        request = {'refreshToken': refresh_token}
+        params = {'method': 'POST',
+                  'merge_data': True}
+        return self._request(url, action, request, **params)
