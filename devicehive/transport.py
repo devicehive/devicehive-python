aa8117c288fc45743554450448178c47246b088f
platon <platon.dimka@gmail.com>
2017-07-04 00:46:32 +0300

    Remove Request and Response classes

--- devicehive/transport.py
+++ devicehive/transport.py
@@ -7,25 +7,3 @@ def init(name, data_format_class, data_format_options, handler_class,
                                                            data_format_options,
                                                            handler_class,
                                                            handler_options)
-
-
-class Request(object):
-    """Request class."""
-
-    def __init__(self, url, action, request, **params):
-        self.action = action
-        self.request = request
-        self.params = params
-        self.params['url'] = url
-
-
-class Response(object):
-    """Response class."""
-
-    def __init__(self, response):
-        self.id = response.pop('requestId')
-        self.action = response.pop('action')
-        self.is_success = response.pop('status') == 'success'
-        self.code = response.pop('code', None)
-        self.error = response.pop('error', None)
-        self.data = response
