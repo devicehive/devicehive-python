bce340828b853cc941b5f0673041d5d63a3fa7bd
Dmitry Platon <platon.dimka@gmail.com>
2017-07-03 18:46:32 +0300

    Add base client handler

---
+++ devicehive/handler.py
@@ -0,0 +1,11 @@
+class Handler(object):
+
+    def __init__(self, api, options):
+        self.api = api
+        self.options = options
+
+    def handle_connected(self):
+        raise NotImplementedError
+
+    def handle_event(self, event):
+        raise NotImplementedError
