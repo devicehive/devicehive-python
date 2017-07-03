69f630b3b05b5e54871f0935758a1f939693bcd1
Dmitry Platon <platon.dimka@gmail.com>
2017-07-03 18:47:06 +0300

    Refactor

--- devicehive/transport.py
+++ devicehive/transport.py
@@ -1,10 +1,11 @@
-def init(name, data_format, data_format_options, handler, handler_options):
+def init(name, data_format_class, data_format_options, handler_class,
+         handler_options):
     transport_class_name = '%sTransport' % name.title()
     transport_module = __import__('devicehive.transports.%s_transport' % name,
                                   fromlist=[transport_class_name])
-    return getattr(transport_module, transport_class_name)(data_format,
+    return getattr(transport_module, transport_class_name)(data_format_class,
                                                            data_format_options,
-                                                           handler,
+                                                           handler_class,
                                                            handler_options)
 
 
