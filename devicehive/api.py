2c92fc38c25301bfaa692be2fac7e9e2d9cc9aa3
Dmitry Platon <platon.dimka@gmail.com>
2017-07-03 18:18:58 +0300

    Add transport type methods

--- devicehive/api.py
+++ devicehive/api.py
@@ -8,6 +8,12 @@ class Api(object):
     def __init__(self, transport):
         self._transport = transport
 
+    def is_http_transport(self):
+        return self._transport.name == 'http'
+
+    def is_websocket_transport(self):
+        return self._transport.name == 'websocket'
+
     def _request(self, url, action, request, **params):
         req = Request(url, action, request, **params)
         response = self._transport.request(req.action(), req.request(),
