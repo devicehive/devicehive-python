4381b2106fbce77d7316359d5c0da3fdedc7058c
platon <platon.dimka@gmail.com>
2017-07-11 00:32:52 +0300

    Rename poll to subscribe and add some fixes

--- devicehive/transports/http_transport.py
+++ devicehive/transports/http_transport.py
@@ -13,6 +13,7 @@ class HttpTransport(Transport):
     RESPONSE_STATUS_KEY = 'status'
     RESPONSE_CODE_KEY = 'code'
     RESPONSE_ERROR_KEY = 'error'
+    RESPONSE_SUBSCRIBE_ID_KEY = 'subscriptionId'
 
     def __init__(self, data_format_class, data_format_options, handler_class,
                  handler_options):
@@ -21,9 +22,8 @@ class HttpTransport(Transport):
         self._connection_thread = None
         self._base_url = None
         self._events_queue = queue.Queue()
-        self._poll_threads = {}
+        self._subscribe_threads = {}
         self._success_codes = [200, 201, 204]
-        self.request_poll_id_key = 'subscriptionId'
 
     def _connection(self, url):
         self._base_url = url
@@ -39,8 +39,8 @@ class HttpTransport(Transport):
 
     def _receive(self):
         while self._connected:
-            for poll_thread in self._poll_threads.values():
-                if not poll_thread.is_alive():
+            for subscribe_thread in self._subscribe_threads.values():
+                if not subscribe_thread.is_alive():
                     return
             if self._events_queue.empty():
                 continue
@@ -51,7 +51,7 @@ class HttpTransport(Transport):
 
     def _close(self):
         self._events_queue = queue.Queue()
-        self._poll_threads = {}
+        self._subscribe_threads = {}
         self._call_handler_method('handle_close')
 
     def _http_request(self, method, url, **params):
@@ -94,48 +94,52 @@ class HttpTransport(Transport):
         response[self.RESPONSE_ERROR_KEY] = response_data.get('message')
         return response
 
-    def _poll_request(self, action, request, **params):
-        poll_id = self._uuid()
-        self._poll_threads[poll_id] = threading.Thread(target=self._poll,
-                                                       args=(action, poll_id,
-                                                             request, params))
-        self._poll_threads[poll_id].daemon = True
-        self._poll_threads[poll_id].name = 'http-transport-poll-%s' % poll_id
-        self._poll_threads[poll_id].start()
+    def _subscribe_request(self, action, request, **params):
+        subscribe_id = self._uuid()
+        subscribe_thread = threading.Thread(target=self._subscribe,
+                                            args=(subscribe_id, action, request,
+                                                  params))
+        subscribe_thread_name = 'http-transport-subscribe-%s' % subscribe_id
+        self._subscribe_threads[subscribe_id] = subscribe_thread
+        self._subscribe_threads[subscribe_id].daemon = True
+        self._subscribe_threads[subscribe_id].name = subscribe_thread_name
+        self._subscribe_threads[subscribe_id].start()
         return {self.REQUEST_ID_KEY: self._uuid(),
                 self.REQUEST_ACTION_KEY: action,
                 self.RESPONSE_STATUS_KEY: self.RESPONSE_SUCCESS_STATUS,
-                self.request_poll_id_key: poll_id}
+                self.RESPONSE_SUBSCRIBE_ID_KEY: subscribe_id}
 
-    def _poll(self, action, poll_id, request, params):
-        data_key = params['data_key']
-        poll_action = params.pop('poll_action')
+    def _subscribe(self, subscribe_id, action, request, params):
+        response_key = params['response_key']
         params_timestamp_key = params.pop('params_timestamp_key', 'timestamp')
-        event_timestamp_key = params.pop('event_timestamp_key', 'timestamp')
-        while self._connected and self._poll_threads.get(poll_id, None):
-            response = self._request(action, request, **params)
-            if response[self.RESPONSE_STATUS_KEY] != \
-                    self.RESPONSE_SUCCESS_STATUS:
+        response_timestamp_key = params.pop('response_timestamp_key',
+                                            'timestamp')
+        response_success_status = self.RESPONSE_SUCCESS_STATUS
+        while self._connected and self._subscribe_threads.get(subscribe_id):
+            response = self._request(action, request.copy(), **params)
+            if response[self.RESPONSE_STATUS_KEY] != response_success_status:
+                # TODO: handle error status here.
                 return
-            events = response[data_key]
+            events = response[response_key]
             if not len(events):
                 continue
-            timestamp = events[-1][event_timestamp_key]
+            timestamp = events[-1][response_timestamp_key]
             if not params.get('params'):
                 params['params'] = {}
             params['params'][params_timestamp_key] = timestamp
-            events = [{self.REQUEST_ACTION_KEY: poll_action,
-                       self.request_poll_id_key: poll_id,
-                       data_key: event} for event in events]
+            # TODO: add action to event.
+            events = [{response_key: event,
+                       self.RESPONSE_SUBSCRIBE_ID_KEY: subscribe_id}
+                      for event in events]
             self._events_queue.put(events)
 
-    def _stop_poll_request(self, action, request):
-        poll_id = request[self.request_poll_id_key]
-        if poll_id not in self._poll_threads:
-            raise HttpTransportRequestException('Polling does not exist')
-        poll_thread = self._poll_threads[poll_id]
-        del self._poll_threads[poll_id]
-        poll_thread.join()
+    def _unsubscribe_request(self, action, request):
+        subscribe_id = request[self.RESPONSE_SUBSCRIBE_ID_KEY]
+        if subscribe_id not in self._subscribe_threads:
+            raise HttpTransportRequestException('Subscription does not exist')
+        subscribe_thread = self._subscribe_threads[subscribe_id]
+        del self._subscribe_threads[subscribe_id]
+        subscribe_thread.join()
         return {self.REQUEST_ID_KEY: self._uuid(),
                 self.REQUEST_ACTION_KEY: action,
                 self.RESPONSE_STATUS_KEY: self.RESPONSE_SUCCESS_STATUS}
@@ -150,27 +154,29 @@ class HttpTransport(Transport):
 
     def send_request(self, action, request, **params):
         self._ensure_connected()
-        poll = params.pop('poll', None)
-        if poll is None:
-            response = self._request(action, request, **params)
+        subscribe = params.pop('subscribe', False)
+        unsubscribe = params.pop('unsubscribe', False)
+        if subscribe:
+            response = self._subscribe_request(action, request, **params)
             self._events_queue.put([response])
             return response[self.REQUEST_ID_KEY]
-        if poll:
-            response = self._poll_request(action, request, **params)
+        if unsubscribe:
+            response = self._unsubscribe_request(action, request)
             self._events_queue.put([response])
             return response[self.REQUEST_ID_KEY]
-        response = self._stop_poll_request(action, request)
+        response = self._request(action, request, **params)
         self._events_queue.put([response])
         return response[self.REQUEST_ID_KEY]
 
     def request(self, action, request, **params):
         self._ensure_connected()
-        poll = params.pop('poll', None)
-        if poll is None:
-            return self._request(action, request, **params)
-        if poll:
-            return self._poll_request(action, request, **params)
-        return self._stop_poll_request(action, request)
+        subscribe = params.pop('subscribe', False)
+        unsubscribe = params.pop('unsubscribe', False)
+        if subscribe:
+            return self._subscribe_request(action, request, **params)
+        if unsubscribe:
+            return self._unsubscribe_request(action, request)
+        return self._request(action, request, **params)
 
     def close(self):
         self._ensure_connected()
