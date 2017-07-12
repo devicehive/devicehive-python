4201b8634acf49ccfba5e66e3ac400a0e9605621
platon <platon.dimka@gmail.com>
2017-07-13 00:52:27 +0300

    Set test object as test function argument

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+from tests.test import Test
+
+
 def pytest_addoption(parser):
     parser.addoption('--transport-urls', action='store', help="Transport urls")
     parser.addoption('--refresh-token', action='store', help="Refresh token")
@@ -6,7 +9,6 @@ def pytest_addoption(parser):
 def pytest_generate_tests(metafunc):
     transport_urls = metafunc.config.option.transport_urls.split(',')
     refresh_token = metafunc.config.option.refresh_token
-    run_options = [{'transport_url': transport_url,
-                    'refresh_token': refresh_token}
-                   for transport_url in transport_urls]
-    metafunc.parametrize('run_options', run_options)
+    tests = [Test(transport_url, refresh_token)
+             for transport_url in transport_urls]
+    metafunc.parametrize('test', tests)
