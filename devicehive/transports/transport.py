5b7401c40a8301cf3bb840ea909972d7ff68a051
platon <platon.dimka@gmail.com>
2017-07-13 08:36:27 +0300

    Move connect, close and join implemetation to transport

--- devicehive/transports/transport.py
+++ devicehive/transports/transport.py
@@ -1,4 +1,5 @@
 import uuid
+import threading
 
 
 class Transport(object):
@@ -13,20 +14,13 @@ class Transport(object):
         self._data_format = data_format_class(**data_format_options)
         self._data_type = self._data_format.data_type
         self._handler = handler_class(self, **handler_options)
+        self._connection_thread = None
         self._exception_info = None
         self._connected = False
 
     @staticmethod
     def _uuid():
         return str(uuid.uuid1())
-
-    def _ensure_not_connected(self):
-        if self._connected:
-            raise TransportConnectionException('Connection has already created')
-
-    def _ensure_connected(self):
-        if not self._connected:
-            raise TransportConnectionException('Connection has not created')
 
     def _encode(self, obj):
         return self._data_format.encode(obj)
@@ -36,6 +30,17 @@ class Transport(object):
 
     def _call_handler_method(self, name, *args):
         getattr(self._handler, name)(*args)
+
+    def _ensure_not_connected(self):
+        if self._connected:
+            raise TransportConnectionException('Connection has already created')
+
+    def _ensure_connected(self):
+        if not self._connected:
+            raise TransportConnectionException('Connection has not created')
+
+    def _connection(self, url, options):
+        raise NotImplementedError
 
     def name(self):
         return self._name
@@ -47,18 +52,24 @@ class Transport(object):
         return self._connected
 
     def connect(self, url, **options):
-        raise NotImplementedError
+        self._ensure_not_connected()
+        self._connection_thread = threading.Thread(target=self._connection,
+                                                   args=(url, options))
+        self._connection_thread.name = '%s-transport-connection' % self._name
+        self._connection_thread.daemon = True
+        self._connection_thread.start()
+
+    def close(self):
+        self._ensure_connected()
+        self._connected = False
+
+    def join(self, timeout=None):
+        self._connection_thread.join(timeout)
 
     def send_request(self, action, request, **params):
         raise NotImplementedError
 
     def request(self, action, request, **params):
-        raise NotImplementedError
-
-    def close(self):
-        raise NotImplementedError
-
-    def join(self, timeout=None):
         raise NotImplementedError
 
 
--- devicehive/transports/websocket_transport.py
+++ devicehive/transports/websocket_transport.py
@@ -13,7 +13,6 @@ class WebsocketTransport(Transport):
                  handler_options):
         Transport.__init__(self, 'websocket', data_format_class,
                            data_format_options, handler_class, handler_options)
-        self._connection_thread = None
         self._websocket = websocket.WebSocket()
         self._pong_received = False
         self._event_queue = []
@@ -29,7 +28,7 @@ class WebsocketTransport(Transport):
             if pong_timeout:
                 ping_thread = threading.Thread(target=self._ping,
                                                args=(pong_timeout,))
-                ping_thread.name = 'websocket-transport-ping'
+                ping_thread.name = '%s-transport-ping' % self._name
                 ping_thread.daemon = True
                 ping_thread.start()
             self._receive()
@@ -86,14 +85,6 @@ class WebsocketTransport(Transport):
         self._websocket.send(self._encode(request), opcode=self._data_opcode)
         return request[self.REQUEST_ID_KEY]
 
-    def connect(self, url, **options):
-        self._ensure_not_connected()
-        self._connection_thread = threading.Thread(target=self._connection,
-                                                   args=(url, options))
-        self._connection_thread.name = 'websocket-transport-connection'
-        self._connection_thread.daemon = True
-        self._connection_thread.start()
-
     def send_request(self, action, request, **params):
         self._ensure_connected()
         return self._send_request(action, request)
@@ -109,13 +100,6 @@ class WebsocketTransport(Transport):
                 return response
             self._event_queue.append(response)
         raise WebsocketTransportRequestException('Timeout occurred')
-
-    def close(self):
-        self._ensure_connected()
-        self._connected = False
-
-    def join(self, timeout=None):
-        self._connection_thread.join(timeout)
 
 
 class WebsocketTransportRequestException(TransportRequestException):
