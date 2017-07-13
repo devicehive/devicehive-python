08d551eaa7ba8d2225b11c3ddf78ea2cdd90c8fc
platon <platon.dimka@gmail.com>
2017-07-13 04:46:15 +0300

    Add test_info.py

---
+++ tests/test_info.py
@@ -0,0 +1,14 @@
+def test_get(test):
+
+    def handle_connect(handler):
+        info = handler.api.get_info()
+        assert isinstance(info['api_version'], str)
+        assert isinstance(info['server_timestamp'], str)
+        if info.get('rest_server_url'):
+            assert info['websocket_server_url'] is None
+            assert isinstance(info['rest_server_url'], str)
+            return
+        assert isinstance(info['websocket_server_url'], str)
+        assert info['rest_server_url'] is None
+
+    test.run(handle_connect)
