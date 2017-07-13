6b9bfa8eeefd67fb996fd831f4e80823d03770eb
platon <platon.dimka@gmail.com>
2017-07-13 15:03:57 +0300

    Replace str with string

--- tests/test_info.py
+++ tests/test_info.py
@@ -1,17 +1,17 @@
-from builtins import str
+from tests.types import string
 
 
 def test_get(test):
 
     def handle_connect(handler):
         info = handler.api.get_info()
-        assert isinstance(info['api_version'], str)
-        assert isinstance(info['server_timestamp'], str)
+        assert isinstance(info['api_version'], string)
+        assert isinstance(info['server_timestamp'], string)
         if info.get('rest_server_url'):
             assert info['websocket_server_url'] is None
-            assert isinstance(info['rest_server_url'], str)
+            assert isinstance(info['rest_server_url'], string)
             return
-        assert isinstance(info['websocket_server_url'], str)
+        assert isinstance(info['websocket_server_url'], string)
         assert info['rest_server_url'] is None
 
     test.run(handle_connect)
