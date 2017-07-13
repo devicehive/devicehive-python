3511a6926ca9831dc1eca22734b56f8121fda49a
platon <platon.dimka@gmail.com>
2017-07-13 04:20:39 +0300

    Add transport_name

--- tests/test.py
+++ tests/test.py
@@ -19,6 +19,10 @@ class Test(object):
     def __init__(self, transport_url, refresh_token):
         self._transport_url = transport_url
         self._refresh_token = refresh_token
+        self._transport_name = DeviceHive.transport_name(self._transport_url)
+
+    def transport_name(self):
+        return self._transport_name
 
     def run(self, handle_connect, handle_event=None):
         handler_options = {'handle_connect': handle_connect,
@@ -27,5 +31,6 @@ class Test(object):
                                  handler_options)
         device_hive.connect(refresh_token=self._refresh_token)
         exception_info = device_hive.join(print_exception=False)
-        if exception_info:
-            raise exception_info[1]
+        if not exception_info:
+            return
+        raise exception_info[1]
