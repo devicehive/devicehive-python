f654bc75c47d3c1aaa93ef70e0d5c2ef8f2bc79e
Dmitry Platon <platon.dimka@gmail.com>
2017-07-14 15:13:23 +0300

    Move transport_url to connect method

--- devicehive/device_hive.py
+++ devicehive/device_hive.py
@@ -6,19 +6,16 @@ import traceback
 class DeviceHive(object):
     """Device hive class."""
 
-    def __init__(self, transport_url, handler_class, handler_options, **params):
-        self._transport_name = self.transport_name(transport_url)
-        assert self._transport_name, 'Unexpected url scheme'
-        self._transport_url = transport_url
+    def __init__(self, handler_class, handler_options):
         self._handler_options = {'handler_class': handler_class,
                                  'handler_options': handler_options}
-        self._transport_options = params.get('transport_options', {})
+        self._transport_name = None
         self._transport = None
 
     def _init_transport(self):
         name = 'devicehive.transports.%s_transport' % self._transport_name
         class_name = '%sTransport' % self._transport_name.title()
-        transport_module = __import__(name, globals(), locals(), [class_name])
+        transport_module = __import__(name, globals(), locals(), [name])
         transport_class = getattr(transport_module, class_name)
         self._transport = transport_class(JsonDataFormat, {}, ConnectionHandler,
                                           self._handler_options)
@@ -30,15 +27,16 @@ class DeviceHive(object):
         if transport_url[0:2] == 'ws':
             return 'websocket'
 
-    def connect(self, login=None, password=None, refresh_token=None,
-                access_token=None):
-        authentication = {'login': login,
-                          'password': password,
-                          'refresh_token': refresh_token,
-                          'access_token': access_token}
+    def connect(self, transport_url, **options):
+        self._transport_name = self.transport_name(transport_url)
+        assert self._transport_name, 'Unexpected transport url scheme'
+        authentication = {'login': options.pop('login', None),
+                          'password': options.pop('password', None),
+                          'refresh_token': options.pop('refresh_token', None),
+                          'access_token': options.pop('access_token', None)}
         self._handler_options['authentication'] = authentication
         self._init_transport()
-        self._transport.connect(self._transport_url, **self._transport_options)
+        self._transport.connect(transport_url, **options)
 
     def join(self, timeout=None, print_exception=True):
         self._transport.join(timeout)
