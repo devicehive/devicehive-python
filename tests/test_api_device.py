33afcc528c366a5484d069c4f67d87cbbfadafd6
platon <platon.dimka@gmail.com>
2017-07-14 03:06:29 +0300

    Add test_get_not_exist function

--- tests/test_api_device.py
+++ tests/test_api_device.py
@@ -1,8 +1,12 @@
+from devicehive.api_object import ApiObjectResponseException
+import pytest
+
+
 def test_get(test):
 
     def handle_connect(handler):
-        device_id = 'test-api-get-device-id-%s' % test.transport_name()
-        name = 'test-api-get-device-name-%s' % test.transport_name()
+        device_id = 'test-api-get-id-%s' % test.transport_name()
+        name = 'test-api-get-name-%s' % test.transport_name()
         data = {'key': 'value'}
         handler.api.put_device(device_id, name=name, data=data)
         device = handler.api.get_device(device_id)
@@ -17,3 +21,19 @@ def test_get(test):
 
     test.run(handle_connect)
 
+
+def test_get_not_exist(test):
+
+    def handle_connect(handler):
+        device_id = 'test-api-get-not-exist-%s' % test.transport_name()
+        try:
+            handler.api.get_device(device_id)
+        except ApiObjectResponseException as api_object_response_exception:
+            # TODO: test for 404 for all transports after bug will be fixed.
+            if test.websocket_transport():
+                assert api_object_response_exception.code == 404
+            if test.http_transport():
+                assert api_object_response_exception.code == 401
+            pytest.xfail('Device does not exist')
+
+    test.run(handle_connect)
