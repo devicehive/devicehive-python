979648062f099b56a35434da997bc4782e1a42dd
platon <platon.dimka@gmail.com>
2017-07-18 17:27:26 +0300

    Add execute_authorized_request method

--- devicehive/token.py
+++ devicehive/token.py
@@ -1,9 +1,13 @@
 from devicehive.api_object import ApiObject
 from devicehive.api_request import ApiRequest
+from devicehive.api_response import ApiResponseException
 
 
 class Token(ApiObject):
     """Token class."""
+
+    AUTHORIZATION_HEADER_NAME = 'Authorization'
+    AUTHORIZATION_HEADER_VALUE_PREFIX = 'Bearer '
 
     def __init__(self, transport, authentication):
         ApiObject.__init__(self, transport)
@@ -19,22 +23,23 @@ class Token(ApiObject):
         pass
 
     def _authenticate(self):
-        if self._websocket_transport():
-            url = None
-            action = 'authenticate'
-            request = {'token': self._access_token}
-            params = {}
-            response = self._request(url, action, request, **params)
-            self._ensure_success_response(response, 'Authentication failure')
+        api_request = ApiRequest(self._transport)
+        if not api_request.websocket_transport():
+            # TODO: remove.
+            headers = {'Authorization': 'Bearer ' + self._access_token}
+            self._authentication_params['headers'] = headers
             return
-        headers = {'Authorization': 'Bearer ' + self._access_token}
-        self._authentication_params['headers'] = headers
+        api_request.set_action('authenticate')
+        api_request.set('token', self._access_token)
+        api_request.execute('Authentication failure')
 
     def _set_authentication_params(self, params):
+        # TODO: remove method.
         for key in self._authentication_params:
             params[key] = self._authentication_params[key]
 
     def authorized_request(self, url, action, request, **params):
+        # TODO: remove method.
         self._set_authentication_params(params)
         response = self._request(url, action, request.copy(), **params)
         if response.success() or response.code() != 401:
@@ -42,6 +47,22 @@ class Token(ApiObject):
         self.authenticate()
         self._set_authentication_params(params)
         return self._request(url, action, request, **params)
+
+    def _set_authorization_header(self, api_request):
+        name = self.AUTHORIZATION_HEADER_NAME
+        value = self.AUTHORIZATION_HEADER_VALUE_PREFIX + self._access_token
+        api_request.set_header(name, value)
+
+    def execute_authorized_request(self, api_request, exception_message):
+        self._set_authorization_header(api_request)
+        try:
+            return api_request.execute(exception_message)
+        except ApiResponseException as api_response_exception:
+            if api_response_exception.code() != 401:
+                raise
+            self.authenticate()
+            self._set_authorization_header(api_request)
+            return api_request.execute(exception_message)
 
     def access_token(self):
         return self._access_token
