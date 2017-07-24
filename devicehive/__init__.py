dcb2bf709e90a1bd5b7db60d74a0b4258c3cbef3
platon <platon.dimka@gmail.com>
2017-07-24 16:02:37 +0300

    Import TransportException

--- devicehive/__init__.py
+++ devicehive/__init__.py
@@ -1,5 +1,6 @@
 from .handler import Handler
 from .device_hive import DeviceHive
+from .transports.transport import TransportException
 from .api_request import ApiRequestException
 from .api_response import ApiResponseException
 from .device import DeviceException
