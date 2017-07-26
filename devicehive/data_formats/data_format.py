9904131d9f28a28f0c71a27e17314ad4087010c5
platon <platon.dimka@gmail.com>
2017-07-26 16:05:46 +0300

    Add name

--- devicehive/data_formats/data_format.py
+++ devicehive/data_formats/data_format.py
@@ -4,8 +4,17 @@ class DataFormat(object):
     TEXT_DATA_TYPE = 'text'
     BINARY_DATA_TYPE = 'binary'
 
-    def __init__(self, data_type):
+    def __init__(self, name, data_type):
+        self._name = name
         self._data_type = data_type
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def data_type(self):
+        return self._data_type
 
     @property
     def text_data_type(self):
