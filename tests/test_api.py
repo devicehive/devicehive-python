d2cd01db6566d5f72991f6466e5a27fe2d887a98
platon <platon.dimka@gmail.com>
2017-08-07 22:51:28 +0300

    Change test_subscribe_insert_commands function

--- tests/test_api.py
+++ tests/test_api.py
@@ -79,7 +79,6 @@ def test_subscribe_insert_commands(test):
 
     def handle_command_insert(handler, command):
         assert command.id == handler.data['command_ids'][0]
-        assert command.command == handler.data['command_names'][0]
         [device.remove() for device in handler.data['devices']]
         handler.disconnect()
 
@@ -87,19 +86,41 @@ def test_subscribe_insert_commands(test):
 
     def handle_connect(handler):
         devices, device_ids, command_ids, command_names = init_devices(handler)
-        device = devices[0]
-        device_1 = handler.api.get_device(device.id)
+        handler.api.subscribe_insert_commands(device_ids, limit=1)
+        set_handler_data(handler, devices, device_ids, command_ids,
+                         command_names)
+
+    def handle_command_insert(handler, command):
+        assert command.id in handler.data['command_ids']
+        [device.remove() for device in handler.data['devices']]
+        handler.disconnect()
+
+    test.run(handle_connect, handle_command_insert)
+
+    def handle_connect(handler):
+        devices, device_ids, command_ids, command_names = init_devices(handler)
+        handler.api.subscribe_insert_commands(device_ids)
         [device.remove() for device in devices]
+
+    def handle_command_insert(*_):
+        assert False
+
+    test.run(handle_connect, handle_command_insert, timeout=5)
+
+    def handle_connect(handler):
+        devices, device_ids, command_ids, command_names = init_devices(handler)
+        handler.api.subscribe_insert_commands(device_ids)
         try:
-            device.subscribe_insert_commands()
+            handler.api.subscribe_insert_commands(device_ids)
             assert False
         except DeviceError:
             pass
-        # TODO: add http transport support after server response will be fixed.
+        [device.remove() for device in devices]
+        # TODO: add http support after server response will be fixed.
         if test.http_transport:
             return
         try:
-            device_1.subscribe_insert_commands()
+            handler.api.subscribe_insert_commands(device_ids)
             assert False
         except ApiResponseError as api_response_error:
             assert api_response_error.code == 403
