189ac1fec271152ef256514981833b43feb47b7b
platon <platon.dimka@gmail.com>
2017-08-10 17:08:39 +0300

    Add NetworkError import

--- devicehive/__init__.py
+++ devicehive/__init__.py
@@ -4,3 +4,4 @@ from .transports.transport import TransportError
 from .api_request import ApiRequestError
 from .api_response import ApiResponseError
 from .device import DeviceError
+from .network import NetworkError
