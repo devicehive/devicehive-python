bf2ef41669bf4f3295185aa029b3cb7cf21dc037
platon <platon.dimka@gmail.com>
2017-08-15 19:40:07 +0300

    Add test_remove function

--- tests/test_api_user.py
+++ tests/test_api_user.py
@@ -1,5 +1,6 @@
 from devicehive.user import User
 from devicehive import UserError
+from devicehive import ApiResponseError
 
 
 def test_save(test):
@@ -26,6 +27,39 @@ def test_save(test):
             user.save()
             assert False
         except UserError:
+            pass
+
+    test.run(handle_connect)
+
+
+def test_remove(test):
+
+    def handle_connect(handler):
+        login = test.generate_id('c-u')
+        password = test.generate_id('c-u')
+        role = User.ADMINISTRATOR_ROLE
+        data = {'k': 'v'}
+        user = handler.api.create_user(login, password, role, data)
+        user_1 = handler.api.get_user(user.id)
+        user.remove()
+        assert not user.id
+        assert not user.login
+        assert not user.last_login
+        assert not user.intro_reviewed
+        assert not user.role
+        assert not user.status
+        assert not user.data
+        try:
+            user.remove()
+            assert False
+        except UserError:
+            pass
+        try:
+            user_1.remove()
+            assert False
+        except ApiResponseError as api_response_error:
+            # TODO: uncomment after server response will be fixed.
+            # assert api_response_error.code == 404
             pass
 
     test.run(handle_connect)
