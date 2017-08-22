6f45e75a93fcc75faff8a167e53c74d603959e18
platon <platon.dimka@gmail.com>
2017-08-22 14:52:35 +0300

    Add setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+
+setup(name='devicehive',
+      version='2.0.0',
+      author='DataArt (http://dataart.com)',
+      author_email='info@devicehive.com',
+      url='https://devicehive.com',
+      license='Apache License 2.0',
+      description='DeviceHive Python connectivity library',
+      long_description=long_description,
+      keywords='iot cloud m2m gateway embedded devicehive',
+      packages=['devicehive', 'devicehive.data_formats', 'devicehive.handlers',
+                'devicehive.transports'],
+      install_requires=['websocket-client>=0.44.0', 'requests>=2.18.1'])
