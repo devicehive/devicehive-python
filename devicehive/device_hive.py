4e5bcbe3515c9fe2831400a5cf81b58b13e5badb
platon <platon.dimka@gmail.com>
2017-10-02 18:32:18 +0300

    Move options

--- devicehive/device_hive.py
+++ devicehive/device_hive.py
@@ -36,15 +36,15 @@ class DeviceHive(object):
     def connect(self, transport_url, **options):
         self._transport_name = self.transport_name(transport_url)
         assert self._transport_name, 'Unexpected transport url scheme'
+        transport_alive_timeout = options.pop('transport_alive_timeout', 0.01)
+        connect_timeout = options.pop('connect_timeout', 30)
+        max_num_connect = options.pop('max_num_connect', 10)
+        connect_interval = options.pop('connect_interval', 1)
         auth = {'login': options.pop('login', None),
                 'password': options.pop('password', None),
                 'refresh_token': options.pop('refresh_token', None),
                 'access_token': options.pop('access_token', None)}
         api_init = options.pop('api_init', True)
-        connect_timeout = options.pop('connect_timeout', 30)
-        max_num_connect = options.pop('max_num_connect', 10)
-        connect_interval = options.pop('connect_interval', 1)
-        transport_alive_timeout = options.pop('transport_alive_timeout', 0.01)
         self._api_handler_options['auth'] = auth
         self._api_handler_options['api_init'] = api_init
         self._init_transport()
