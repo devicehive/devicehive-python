d939b37b65689f41a8e01238dfa0964a5ee2d97b
platon <platon.dimka@gmail.com>
2017-10-04 14:02:29 +0300

    Change test_remove function

--- tests/test_api_network.py
+++ tests/test_api_network.py
@@ -30,26 +30,23 @@ def test_save(test):
 
 
 def test_remove(test):
-
-    def handle_connect(handler):
-        name = test.generate_id('n-r')
-        description = '%s-description' % name
-        network = handler.api.create_network(name, description)
-        network_1 = handler.api.get_network(network.id)
-        network.remove()
-        assert not network.id
-        assert not network.name
-        assert not network.description
-        try:
-            network.remove()
-            assert False
-        except NetworkError:
-            pass
-        try:
-            network_1.remove()
-            assert False
-        except ApiResponseError as api_response_error:
-            assert api_response_error.code == 404
-
     test.only_admin_implementation()
-    test.run(handle_connect)
+    device_hive_api = test.device_hive_api()
+    name = test.generate_id('n-r')
+    description = '%s-description' % name
+    network = device_hive_api.create_network(name, description)
+    network_1 = device_hive_api.get_network(network.id)
+    network.remove()
+    assert not network.id
+    assert not network.name
+    assert not network.description
+    try:
+        network.remove()
+        assert False
+    except NetworkError:
+        pass
+    try:
+        network_1.remove()
+        assert False
+    except ApiResponseError as api_response_error:
+        assert api_response_error.code == 404
