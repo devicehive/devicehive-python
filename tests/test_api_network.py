2716818ffe01f0bd59c199fcecaa184839c31436
platon <platon.dimka@gmail.com>
2017-10-04 14:00:15 +0300

    Change test_save function

--- tests/test_api_network.py
+++ tests/test_api_network.py
@@ -3,33 +3,30 @@ from devicehive import ApiResponseError
 
 
 def test_save(test):
-
-    def handle_connect(handler):
-        name = test.generate_id('n-s')
-        description = '%s-description' % name
-        network = handler.api.create_network(name, description)
-        name = test.generate_id('n-s')
-        description = '%s-description' % name
-        network.name = name
-        network.description = description
-        network.save()
-        network_1 = handler.api.get_network(network.id)
-        network.remove()
-        try:
-            network.save()
-            assert False
-        except NetworkError:
-            pass
-        try:
-            network_1.save()
-            assert False
-        except ApiResponseError as api_response_error:
-            # TODO: uncomment after server response will be fixed.
-            # assert api_response_error.code == 404
-            pass
-
     test.only_admin_implementation()
-    test.run(handle_connect)
+    device_hive_api = test.device_hive_api()
+    name = test.generate_id('n-s')
+    description = '%s-description' % name
+    network = device_hive_api.create_network(name, description)
+    name = test.generate_id('n-s')
+    description = '%s-description' % name
+    network.name = name
+    network.description = description
+    network.save()
+    network_1 = device_hive_api.get_network(network.id)
+    network.remove()
+    try:
+        network.save()
+        assert False
+    except NetworkError:
+        pass
+    try:
+        network_1.save()
+        assert False
+    except ApiResponseError as api_response_error:
+        # TODO: uncomment after server response will be fixed.
+        # assert api_response_error.code == 404
+        pass
 
 
 def test_remove(test):
