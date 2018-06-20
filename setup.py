d7f03d84e934d4b7171de76df327003a8c4d7294
platon <platon.dimka@gmail.com>
2018-06-20 13:43:06 +0300

    Increase version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ except(IOError, ImportError):
 
 
 setup(name='devicehive',
-      version='2.1.5',
+      version='2.1.6',
       author='DataArt (http://dataart.com)',
       author_email='info@devicehive.com',
       url='https://devicehive.com',
